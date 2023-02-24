74a6c3d7ce93c21c85098c94cd5bf4654f480762
Saifuddin <106925140+Mohammad-Saifuddin@users.noreply.github.com>
2023-02-24 11:49:30 +0530

    Update splitting-data.py

--- splitting-data.py
+++ splitting-data.py
@@ -6,3 +6,29 @@ train = data.take(train_size)
 remaining = data.skip(train_size)
 val = remaining.take(val_size)
 test = remaining.skip(val_size)
+
+
+test_iter = test.as_numpy_iterator()
+test_set = {"images":np.empty((0,224,224,3)), "labels":np.empty(0)}
+while True:
+    try:
+        batch = test_iter.next()
+        test_set['images'] = np.concatenate((test_set['images'], batch[0]))
+        test_set['labels'] = np.concatenate((test_set['labels'], batch[1]))
+    except:
+        break
+y_test = test_set['labels']
+
+
+def evaluate_model(model):
+    model.evaluate(test)
+    y_pred = np.argmax(model.predict(test_set['images']), 1)
+    print(classification_report(y_test, y_pred, target_names = class_names))
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(10, 8))
+    sn.heatmap(cm, annot=True)
+    plt.xticks(np.arange(4)+.5, class_names, rotation=90)
+    plt.yticks(np.arange(4)+.5, class_names)
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.title("Confusion Matrix")
