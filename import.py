25cb56e1bb36aee567e2aa714c75a7807bf4bc5f
sinansinu07 <mohammedsinanchinuu07@lbscek.ac.in>
2023-05-08 15:02:27 +0530

    early stopping fn

--- import.py
+++ import.py
@@ -81,7 +81,16 @@ model.summary()
 
 #training the model
 
-    history = model.fit(train, validation_data=val, epochs = 10)
+    history = model.fit(train, validation_data=val, epochs = 100)
+    tf.keras.callbacks.EarlyStopping(
+        monitor="val_loss",
+        min_delta=0,
+        patience=5,
+        verbose=0,
+        mode="auto",
+        baseline=None,
+        restore_best_weights=False,
+        start_from_epoch=0,)
 
 #checking the performance of the model
 
